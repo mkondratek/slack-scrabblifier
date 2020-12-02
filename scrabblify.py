27eba242e1bd318b80a9b3ae3face2b05ebf9fbe
mkondratek <mkondratek@virtuslab.com>
2020-12-02 16:56:26 +0100

    Add numbers support

--- scrabblify.py
+++ scrabblify.py
@@ -30,26 +30,30 @@ german=dict()
 german.update(eszett)
 german.update(umlauts)
 
-scrabble_name_prefix=":scrabble-"
-scrabble_name_suffix=":"
+scrabble_name_prefix="scrabble-"
 umlaut_infix="-umlaut"
 with_acute_name_infix="-with-acute"
 with_ogonek_name_infix="-with-ogonek"
 with_overdot_name_infix="-with-overdot"
 with_stroke_name_infix="-with-stroke"
 
-
-if len(sys.argv) < 2:
-    raise ValueError("missing argument")
-else: 
-    scrabblified = ''.join([scrabblify(c) for c in sys.argv[1].lower()])
-    print(scrabblified)
-
+numbers={'0': "zero",
+         '1': "one",
+         '2': "two",
+         '3': "three",
+         '4': "four",
+         '5': "five",
+         '6': "siz",
+         '7': "seven",
+         '8': "eight",
+         '9': "nine"}
 
 def scrabblify(c, allow_interpunction=True):
     c_str=str(c)
     if c in string.ascii_lowercase:
-        id=c_str
+        id=scrabble_name_prefix + str(c)
+    elif c in numbers.keys():
+        id=numbers[c]
     elif c in polish.keys():
         c_str=polish[c]
         if c in polish_with_acute_to_base.keys():
@@ -62,6 +66,7 @@ def scrabblify(c, allow_interpunction=True):
             id=c_str + with_stroke_name_infix
         else:
             raise ValueError("unknown polish character: %c" % c)
+        id=scrabble_name_prefix + id 
     elif c in german.keys():
         c_str=german[c]
         if c in umlauts.keys():
@@ -70,13 +75,19 @@ def scrabblify(c, allow_interpunction=True):
             id=c_str
         else:
             raise ValueError("unknown german character: %c" % c)
+        id=scrabble_name_prefix + id 
     elif c == ' ':
-        id="blank" 
+        id=scrabble_name_prefix + "blank" 
     elif c in interpunction.keys() and allow_interpunction:
         return ":" + interpunction[c] + ":"
     else:
         raise ValueError("unknown character: %c" % c)
     
-    return scrabble_name_prefix + id + scrabble_name_suffix
+    return ":%s:" % id
 
+if len(sys.argv) < 2:
+    raise ValueError("missing argument")
+else: 
+    scrabblified = ''.join([scrabblify(c) for c in sys.argv[1].lower()])
+    print(scrabblified)
 
